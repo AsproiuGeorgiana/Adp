e85426774180dbbad7387c669542ebbf043ecf57
AsproiuGeorgiana <asproiuac@gmail.com>
2017-07-13 10:39:45 +0300

    Update SourceB.cpp

--- Lab2/SourceB.cpp
+++ Lab2/SourceB.cpp
@@ -1,75 +1,81 @@
-# include <iostream>
 #include "mpi.h"
-
-#define SIZE 24
-
+#include <time.h>
+#include <iostream>
 using namespace std;
+
+#define SIZE 12
+#define NR 0
 
 int main(int argc, char *argv[])
 {
-	const int searchedNumber = 7;
+	int rank, nrProcs, slice, indexVpoz;
+	int array[SIZE], segment[SIZE], Vpoz[SIZE], finalFound[SIZE];
+	bool display = false;
 
-	int numProc;
-	int slice;
-	int v[SIZE];
-	int nrIndex[SIZE];
-	int resultIndex;
-	int maxIndex;
+	srand((unsigned int)time(NULL));
 
-	MPI_Init(&argc, &argv);	
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &nrProcs);
 
-	int procId;
-	MPI_Comm_rank(MPI_COMM_WORLD, &procId);
+	slice = SIZE / nrProcs;
 
-
-	if (procId == 0)
+	//avem grija ca toate bucatile sa fie egale
+	if (SIZE % nrProcs != 0)
+		++slice;
+	//blocheaza procesul curent pana cand toate celelalte procese din communicator isi termina rutina
+	MPI_Barrier(MPI_COMM_WORLD);
+	
+	//initializeaza array 
+	if (rank == 0)
 	{
-		for (int i = 0; i < SIZE; ++i)
+		cout << "Array elements are:\n";
+		for (int i = 0; i < SIZE; i++)
 		{
-			v[i] = rand() % 10;
-			nrIndex[i] = -1;
-			cout << v[i] << " ";
+			array[i] = rand() % 5;
+			cout << array[i] << " ";
+			finalFound[i] = -1;
 		}
-		cout << endl << endl;
+		//cout << "Searched number is: ";
+		//cin >> NR;
+		//cout << "\n";
 	}
+	//trimite seg egale de date de la root catre celelalte proc din communicator
+	MPI_Scatter(array, slice, MPI_INT, segment, slice, MPI_INT, 0, MPI_COMM_WORLD);
+	cout << "\n\nRank: " << rank << ". Segement size: " << slice << "\n";
+	//initializare 
+	for (int i = 0; i < slice; i++)
+		Vpoz[i] = -1;
 
-	MPI_Barrier(MPI_COMM_WORLD);
-	MPI_Bcast(v, SIZE, MPI_INT, 0, MPI_COMM_WORLD);
-
-	MPI_Comm_size(MPI_COMM_WORLD, &numProc);
-	slice = SIZE / numProc;
-
-	int start = slice * procId;
-	int end = start + slice;
-	int count = 0;
-
-	for (int i = start; i < end; ++i)
+	indexVpoz = 0;
+	for (int i = 0; i < slice; i++)
 	{
-		if (v[i] == searchedNumber)
-			nrIndex[count++] = i;
+		if (segment[i] == NR)
+		{
+			Vpoz[++indexVpoz] = i + rank * slice;
+		}
 	}
-	for (int i = 0; i < count; i++){
-		cout << "Procesul " << procId << " a gasit numarul pe pozitiile : " << nrIndex[i] << " ";
-	}
-	maxIndex = nrIndex[0];
-	for (int i = 0; i <= SIZE; ++i)
+	//reuneste toate pozitiile in vect finalFound prin intermediul Gather
+	MPI_Gather(Vpoz, slice, MPI_INT, finalFound, slice, MPI_INT, 0, MPI_COMM_WORLD);
+
+	if (rank == 0)
 	{
-		if (nrIndex[i] > maxIndex)
-			maxIndex = nrIndex[i];
+		for (int i = 0; i < SIZE ; i++)
+		{
+			if (finalFound[i] >= 0)
+			{
+				if (!display)
+					cout << "\n\nNumber found on the follwing position(s): \n";
+				display = true;
+				cout << finalFound[i] << " ";
+			}
+		}
+		if (!display)
+			cout << "\n\nNumber not found.";
 	}
-
-	MPI_Barrier(MPI_COMM_WORLD);
-	MPI_Reduce(&maxIndex, &resultIndex, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
-
-	if (procId == 0)
-	{
-		if (resultIndex < 0)
-			cout << "Element not found!" << endl;
-		else
-			cout << "\nMaximum position for " << searchedNumber << " is : " << resultIndex << endl;
-	}
-
+	
 	MPI_Finalize();
 	cout << endl;
+	system("pause");
 	return 0;
 }
