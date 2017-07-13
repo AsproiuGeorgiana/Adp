d685e16c177af2b9d7ed4cdd7ffb4ce919f2dc81
AsproiuGeorgiana <asproiuac@gmail.com>
2017-07-13 09:49:43 +0300

    Update main.c

--- OpenMP/main.c
+++ OpenMP/main.c
@@ -1,65 +1,67 @@
 #include <omp.h>
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-/*#define n s
-#define p s
-#define m s
-float a[n][p];
-float b[p][m];
-float c[n][m];
-int main() {
-    unsigned long i,j,k;
-    for(i=0; i<n; i++)
-        for(k=0; k<p ; k++)
-            a[i][k]= rand();
-    for(k=0; k<p; k++)
-        for(j=0; j<m ; j++)
-            a[k][j]= rand();
-    for(i=0; i<n; i++)
-        for(j=0; j<m ; j++)
-            a[i][j]= rand();
-#pragma omp parallel shared(a,b,c) private(i,j,k) //a,c,b are shared among the threads & i,j,k are private to each threads
+#define NRA 70                 //numarul de linii din matricea A
+#define NCA 30                 //numarul de coloane sin matricea A si numarul de linii din matricea B
+#define NCB 7                  //numarul de coloane din matricea B
+
+int main (int argc, char *argv[])
 {
-    #pragma omp schedule (static)
-     for(i=0; i<n; i++) {
-        for(j=0; j<m ; j++) {
-            for(k=0; k<p; k++) {
-                c[i][j]+= a[i][k] * b[k][j];
-            }
-        }
-     }
-    return 0;
-}
-*/
+int	tid, nthreads, i, j, k, chunk;
+int	a[NRA][NCA],           //matricea A
+    b[NCA][NCB],           //matricea B
+    c[NRA][NCB];           //matricea C=A*B
 
+chunk = 10;
+srand(time(NULL));
 
-
-float a[1000][1000];
-float b[1000][1000];
-float c[1000][1000];
-
-int main()
-{
-    int i,j,k;
-    // Initialize buffers.
-    for ( i = 0; i < 1000; ++i) {
-        for ( j = 0; j < 1000; ++j) {
-            a[i][j] = (float)i + j;
-            b[i][j] = (float)i - j;
-            c[i][j] = 0.0f;
-        }
+#pragma omp parallel shared(a,b,c,nthreads,chunk) private(tid,i,j,k)
+  {
+  tid = omp_get_thread_num();
+  if (tid == 0)
+    {
+    nthreads = omp_get_num_threads();
+    printf("Inmultirea matricilor cu %d fire de executie\n",nthreads);
+    printf("Initializare matrici...\n");
     }
-
-    // Compute matrix multiplication.
-    // C <- C + A x B
-    #pragma omp parallel for default(none) shared(a,b,c)
-    for ( i = 0; i < 1000; ++i) {
-        for (j = 0; j < 1000; ++j) {
-            for (k = 0; k < 1000; ++k) {
-                c[i][j] += a[i][k] * b[k][j];
-            }
-        }
+  //Initializare matrici
+  #pragma omp for schedule (static, chunk)
+  for (i=0; i<NRA; i++)
+    for (j=0; j<NCA; j++){
+      a[i][j]=rand()%10;
+      //printf("%d  ",a[i][j]);
     }
+  #pragma omp for schedule (static, chunk)
+  for (i=0; i<NCA; i++)
+    for (j=0; j<NCB; j++){
+      b[i][j]=rand()%10+10;
+      //printf("%d  ",b[i][j]);
+    }
+  #pragma omp for schedule (static, chunk)
+  for (i=0; i<NRA; i++)
+    for (j=0; j<NCB; j++)
+      c[i][j]= 0;
 
-    return 0;
+  printf("Firul %d a inceput inmultirea matricilor...\n",tid);
+  #pragma omp for schedule (static, chunk)
+  for (i=0; i<NRA; i++)
+    {
+    printf("Firul=%d a facut linia=%d\n",tid,i);
+    for(j=0; j<NCB; j++)
+      for (k=0; k<NCA; k++)
+        c[i][j] += a[i][k] * b[k][j];
+    }
+  }   //sfarsitul regiunii paralele
+
+printf("Matricea rezultat C:\n");
+for (i=0; i<NRA; i++)
+  {
+  for (j=0; j<NCB; j++)
+    printf("%d   ", c[i][j]);
+  printf("\n");
+  }
+
+return 0;
 }
